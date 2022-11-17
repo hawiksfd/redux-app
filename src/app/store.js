aa263f1f46d7daf194649dc2f6e8de194a8acc5d
hawik <hawiksyaef@gmail.com>
2022-11-17 15:04:51 +0700

    create store

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import productSlice from "../features/productSlice.js";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    product: productSlice,
+  },
 });
